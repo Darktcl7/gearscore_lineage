cb9d5020cdde00d745d0e1d6a515eaa8bd76d067
Darktcl7 <165794588+Darktcl7@users.noreply.github.com>
2026-04-14 21:14:59 +0800

    Fix syntax algo

--- helper_fix_dkp.py
+++ helper_fix_dkp.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alto.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 django.setup()
 
 from dkp.models import DKPProfile, DKPLog
